e7d019b963f9f91c85eef2f4c40f32b7d2900286
julienmalard <julien.malard@mail.mcgill.ca>
2022-11-27 13:54:08 +0700

    Initial work

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <app-bar />
+    <v-main>
+      <HelloWorld />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+  import HelloWorld from '@/components/HelloWorld.vue'
+  import appBar from '@/components/appBar.vue'
+</script>
---
+++ src/components/appBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-app-bar>
+        <template v-slot:prepend>
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        </template>
+
+        <v-app-bar-title>Riff.cc</v-app-bar-title>
+
+        <template v-slot:append>
+            <v-btn icon="mdi-cog"></v-btn>
+            <v-btn icon="mdi-dots-vertical"></v-btn>
+        </template>
+    </v-app-bar>
+</template>
