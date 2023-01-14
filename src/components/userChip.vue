afff56c0f913d8d5483ca9ad05f37234f4948b58
julienmalard <julien.malard@mail.mcgill.ca>
2023-01-14 15:16:31 +0100

    Externalise name translation

--- src/components/userChip.vue
+++ src/components/userChip.vue
@@ -8,6 +8,8 @@
 import Riff from '@/plugins/riff/riff';
 import { computed, inject, onMounted, onUnmounted, ref } from 'vue';
 
+import { selectTranslation } from '@/utils';
+
 const riff: Riff = inject("riff")!;
 
 const props = defineProps<{accountId: string}>();
@@ -15,10 +17,7 @@ const props = defineProps<{accountId: string}>();
 const names = ref<{[language: string]: string}>();
 
 const displayName = computed(()=>{
-    // Constellation has a multilingual-centric structure, but for now the Riff.CC site is monolingual,
-    // so we'll just use any name. Once Riff.CC has an internationalised interface, we can match displayed
-    // usernames with the viewer's chosen site language here.
-    return names.value && Object.keys(names.value).length ? Object.values(names.value)[0] : "Anonymous"
+    return selectTranslation(names.value) || "Anonymous"
 });
 
 let forgetNames: (() => Promise<void>)|undefined = undefined;
