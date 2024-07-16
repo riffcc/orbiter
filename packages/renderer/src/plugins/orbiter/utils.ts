ca58b0cf79c380d8a55be64cb19b660c3bc3c86f
julienmalard <julien.malard@mail.mcgill.ca>
2024-07-16 17:52:34 +0200

    Inject Orbiter, not Constellation here

--- packages/renderer/src/plugins/orbiter/utils.ts
+++ packages/renderer/src/plugins/orbiter/utils.ts
@@ -3,7 +3,7 @@ import { inject } from 'vue';
 import type Orbiter from './orbiter';
 
 export const useOrbiter = () => {
-  const orbiter = inject<Orbiter>('constl');
+  const orbiter = inject<Orbiter>('orbiter');
   if (orbiter) return {orbiter};
   throw new Error('Orbiter not found. Did you register the Orbiter plugin with Vue?');
 };
