95313f1c22a59582d94477c1baf0225d2ee40069
benya7 <bitup.games@gmail.com>
2024-10-01 18:36:59 +0200

    update: views

---
+++ packages/renderer/src/views/buildingPage.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-container>
+    <p>Working. Coming soon..</p>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ packages/renderer/src/views/uploadPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <v-sheet
+      width="480px"
+      class="px-8 pb-16 pt-10 mx-auto"
+    >
+      <new-release-form />
+    </v-sheet>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import NewReleaseForm from '/@/components/releases/newReleaseForm.vue';
+</script>
+
