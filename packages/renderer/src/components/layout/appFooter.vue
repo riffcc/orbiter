7302218de06303ffc215e630b5e083c4926d56d7
benya7 <bitup.games@gmail.com>
2024-10-02 19:03:09 +0200

    fix: merge and minnors styles

--- packages/renderer/src/components/layout/appFooter.vue
+++ packages/renderer/src/components/layout/appFooter.vue
@@ -21,7 +21,7 @@
             ></v-img>
             <v-list-item
               subtitle="Riff.CC is a dynamic platform for music, videos, and creative content. Discover, share, and enjoy a wide variety of multimedia in one place."
-              class="pa-0"
+              class="pa-0 mb-2"
             ></v-list-item>
             <v-list-item
               subtitle="The Riff.CC project is a collaborative effort to create a free and open-source platform for music, videos, and creative content. We believe in the power of community and the importance of sharing knowledge and experiences. So Riff.CC's software allows you to build whatever you want, and link it with other sites that share your interests, allowing you to cooperatively build up a content library while each retains sovereignty over their library's specific content."
@@ -31,7 +31,7 @@
         </v-col>
         <v-col
           cols="12"
-          md="6"
+          md="9"
         >
           <v-sheet
             class="d-flex flex-wrap justify-space-evenly"
@@ -47,64 +47,15 @@
                   :key="item.label"
                   :subtitle="item.label"
                   :ripple="false"
-                  class="mb-2 px-0"
+                  class="mb-2 pl-1"
                   min-height="12px"
                   height="24px"
                   @click="() => $router.push(item.path)"
-                >
-                  <template #prepend>
-                    <div class="pr-2 d-flex pb-1">
-                      <v-icon
-                        icon="fas fa-circle"
-                        size="8px"
-                        color="primary"
-                      ></v-icon>
-                    </div>
-                  </template>
-                </v-list-item>
+                ></v-list-item>
               </v-list>
             </div>
           </v-sheet>
         </v-col>
-<<<<<<< HEAD
-        <v-col
-          cols="12"
-          md="3"
-        >
-          <v-sheet>
-            <p class="text-h5 mb-4">Download App</p>
-            <v-list-item
-              subtitle="lorem ispusa asdmasd msmd lsdk ls askwe"
-              class="px-0 mb-4"
-              min-height="25px"
-            >
-              <template #prepend>
-                <div class="pr-2 d-flex pb-1">
-                  <v-icon
-                    icon="fas fa-circle"
-                    size="8px"
-                    color="primary"
-                  ></v-icon>
-                </div>
-              </template>
-            </v-list-item>
-            <div class="d-flex">
-              <v-img
-                width="120px"
-                inline
-                class="mr-2"
-                src="/mock/app-store.png"
-              ></v-img>
-              <v-img
-                width="120px"
-                inline
-                src="/mock/play-store.png"
-              ></v-img>
-            </div>
-          </v-sheet>
-        </v-col>
-=======
->>>>>>> 80d8f56 (Remove "apps" since we do not have those yet)
       </v-row>
     </v-container>
     <v-btn
@@ -136,30 +87,8 @@
 </template>
 
 <script setup lang="ts">
-<<<<<<< HEAD
 import { navigationMap } from '/@/constants/navigation';
 
-=======
-const listItems = {
-  about: [
-    'GitHub',
-    'Movie',
-    'Tv',
-    'Music',
-    'AudioBooks',
-  ],
-  project: [
-    'Terms Of Use',
-    'Contact Us',
-    'Our Team',
-  ],
-  help: [
-    'Privacy Policy',
-    'About the Riff.CC Project',
-    'Contact Us',
-  ],
-};
->>>>>>> 77fd247 (Experimental text guff, cleaning up language and links)
 const scrollToTop = () => {
   window.scrollTo({
     top: 0,
