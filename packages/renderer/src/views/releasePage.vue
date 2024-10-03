6e080b7589c6fbc9b595ec5a7d2c1374e3f6fd7b
benya7 <bitup.games@gmail.com>
2024-10-03 06:20:20 +0200

    add: hardcoded contentCID and very basic video player

---
+++ packages/renderer/src/views/releasePage.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <v-sheet
+      min-height="75vh"
+    >
+      <video
+        v-if="props.category === 'video'"
+        :src="`https://${IPFS_GATEWAY}/ipfs/${props.contentCID}`"
+        controls
+        width="100%"
+        height="100%"
+      >
+        Your browser does not support the video tag.
+      </video>
+    </v-sheet>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps({
+  contentCID: {
+    required: true,
+    type: String,
+  },
+  category: {
+    required: true,
+    type: String,
+  },
+});
+
+const IPFS_GATEWAY = 'cdn.riff.cc';
+
+</script>
+
