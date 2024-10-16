154412aaafadeb78fa1cf52fce2bf91307165561
julienmalard <julien.malard@mail.mcgill.ca>
2024-10-16 21:23:00 +0530

    Add navigation to account page

--- packages/renderer/src/constants/navigation.ts
+++ packages/renderer/src/constants/navigation.ts
@@ -18,31 +18,32 @@ type NavigationMap = {
 export const navigationMap: NavigationMap = {
   appBar: {
     normal: [
-      { label: 'Home', path: '/' },
-      { label: 'Music', path: '/music' },
-      { label: 'Movies', path: '/movies' },
-      { label: 'TV Shows', path: '/tv-shows' },
+      {label: 'Home', path: '/'},
+      {label: 'Music', path: '/music'},
+      {label: 'Movies', path: '/movies'},
+      {label: 'TV Shows', path: '/tv-shows'},
     ],
     authenticated: [
-      { label: 'Upload', path: '/upload' },
+      {label: 'Upload', path: '/upload'},
+      {label: 'Account', path: '/account'},
       // { label: 'Admin', path: '/admin' },
     ],
   },
   appFooter: {
     explore: [
-      { label: 'Home', path: '/' },
-      { label: 'Movies', path: '/movies' },
-      { label: 'TV Shows', path: '/tv-shows' },
-      { label: 'Music', path: '/music' },
-      { label: 'AudioBooks', path: '/audiobooks' },
+      {label: 'Home', path: '/'},
+      {label: 'Movies', path: '/movies'},
+      {label: 'TV Shows', path: '/tv-shows'},
+      {label: 'Music', path: '/music'},
+      {label: 'AudioBooks', path: '/audiobooks'},
     ],
     company: [
-      { label: 'Terms of Use', path: '/terms' },
-      { label: 'About the Riff.CC Project', path: '/about' },
+      {label: 'Terms of Use', path: '/terms'},
+      {label: 'About the Riff.CC Project', path: '/about'},
     ],
     help: [
-      { label: 'Privacy Policy', path: '/privacy-policy' },
-      { label: 'Contact Us', path: '/contact' },
+      {label: 'Privacy Policy', path: '/privacy-policy'},
+      {label: 'Contact Us', path: '/contact'},
     ],
   },
 };
