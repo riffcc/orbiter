84a71b79e1b6dc60545eeea5fe965bfba55f70fc
julienmalard <julien.malard@mail.mcgill.ca>
2024-10-17 14:59:48 +0530

    Make swarm private by default

--- packages/renderer/src/views/uploadPage.vue
+++ packages/renderer/src/views/uploadPage.vue
@@ -4,11 +4,27 @@
       width="480px"
       class="px-8 pb-16 pt-10 mx-auto"
     >
-      <new-release-form />
+      <new-release-form v-if="canUpload" />
+      <v-alert
+        v-else
+        type="info"
+        class="mt-4"
+        color="black"
+        text-color="white"
+      >
+        You aren't currently authorised to add releases to this instance of Riff.CC.
+      </v-alert>
     </v-sheet>
   </v-container>
 </template>
 
 <script setup lang="ts">
+import { suivre as follow} from '@constl/vue';
+import {useOrbiter} from '/@/plugins/orbiter/utils';
+
 import NewReleaseForm from '/@/components/releases/newReleaseForm.vue';
+
+const {orbiter} = useOrbiter();
+const canUpload = follow(orbiter.followCanUpload.bind(orbiter));
+
 </script>
