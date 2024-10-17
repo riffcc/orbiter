a3dc39bd936650f5581230f14b16f16eb43a92d2
julienmalard <julien.malard@mail.mcgill.ca>
2024-10-17 14:45:43 +0530

    Fix router on Electron

--- packages/renderer/src/indexWeb.ts
+++ packages/renderer/src/indexWeb.ts
@@ -1,11 +1,9 @@
 import {createApp} from 'vue';
 import App from './App.vue';
 import {inscrireExtentions} from './plugins/inscription/browser.js';
-import routeur from './plugins/router';
 
 const app = createApp(App);
 
 inscrireExtentions(app);
-app.use(routeur);
 
 app.mount('#app');
