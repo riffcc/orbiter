0dbd84e5ada8cc2067213a647be8228b9a1eb783
benya7 <bitup.games@gmail.com>
2024-10-21 18:47:02 +0200

    fix: playerVolume instance state

--- packages/renderer/src/composables/playerVolume.ts
+++ packages/renderer/src/composables/playerVolume.ts
@@ -1,23 +1,21 @@
 import {ref, type Ref} from 'vue';
 
-const volume = ref(1);
-
-const mute = () => {
-  volume.value = 0;
-};
-const unmute = () => {
-  volume.value = 1;
-};
-const toggleVolume = () => {
-  volume.value > 0 ? mute() : unmute();
-};
-
 export const usePlayerVolume = function (): {
   mute: () => void;
   unmute: () => void;
   toggleVolume: () => void;
   volume: Ref<number>;
 } {
+  const volume = ref(1);
+  const mute = () => {
+    volume.value = 0;
+  };
+  const unmute = () => {
+    volume.value = 1;
+  };
+  const toggleVolume = () => {
+    volume.value > 0 ? mute() : unmute();
+  };
   return {
     mute,
     unmute,
