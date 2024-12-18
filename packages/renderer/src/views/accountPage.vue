95f7f30847c09180bffe8a718928591a0e6bd9d9
julienmalard <julien.malard@mail.mcgill.ca>
2024-12-18 19:51:51 +0530

    Show account info and status

--- packages/renderer/src/views/accountPage.vue
+++ packages/renderer/src/views/accountPage.vue
@@ -11,30 +11,62 @@
       :label="`Static mode ${staticModeSwitch ? 'on' : 'off'}`"
       :color="staticModeSwitch ? 'primary' : 'secondary'"
     />
+    <p>
+      Account id: {{ accountId }}
+    </p>
+    <p>
+      Device id: {{ deviceId }}
+    </p>
+    <p>
+      Account status: {{ accountStatus }} ({{ statusExplanation }})
+    </p>
   </v-container>
 </template>
 <script setup lang="ts">
 import {computed, ref, watchEffect} from 'vue';
 import {selectTranslation} from '/@/utils';
 
-import {suivre as follow} from '@constl/vue';
+import {suivre as follow, obt} from '@constl/vue';
 import {useUserProfilePhoto} from '/@/components/users/utils';
 import {useDevStatus} from '/@/composables/devStatus';
 import {useOrbiter} from '/@/plugins/orbiter/utils';
 
 const {orbiter} = useOrbiter();
 
-const props = defineProps<{accountId?: string}>();
-
 // User name
-const names = follow(orbiter.listenForNameChange, {accountId: computed(() => props.accountId)});
+const names = follow(orbiter.listenForNameChange);
 
 const displayName = computed(() => {
   return selectTranslation(names.value) || 'Anonymous';
 });
 
+// Account id
+const accountId = follow(orbiter.constellation.suivreIdCompte);
+const deviceId = obt(orbiter.constellation.obtIdDispositif);
+
 // User avatar
-const userAvatar = useUserProfilePhoto(props.accountId);
+const userAvatar = useUserProfilePhoto(accountId.value);
+
+// Account status
+const moderator = follow(orbiter.listenToIsModerator);
+const canUpload = follow(orbiter.followCanUpload);
+const accountStatus = computed(()=>{
+  return moderator.value || (canUpload.value ? 'MEMBER' : 'GUEST');
+});
+const statusExplanation = computed(()=>{
+  switch (accountStatus.value) {
+    case 'ADMIN':
+      return 'Can moderate content and invite other moderators or administrators.';
+    case 'MODERATOR':
+      return 'Can moderate content.';
+    case 'MEMBER':
+      return 'Can add content.';
+    case 'GUEST':
+      return 'View-only access to site.';
+    default:
+      return 'Unknown role';
+  }
+});
 
 // Dev static mode
 const {status} = useDevStatus();
